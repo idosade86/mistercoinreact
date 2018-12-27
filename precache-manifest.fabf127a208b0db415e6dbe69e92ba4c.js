937b7dc8e3a61323a1cd3513568246c1036e8ef5
Ido <idokestin@gmail.com>
2018-12-27 16:37:02 +0200

    Updates

---
+++ precache-manifest.fabf127a208b0db415e6dbe69e92ba4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d80b6c672fc519472fb4",
+    "url": "/mistercoinreact/static/css/main.2815c79a.chunk.css"
+  },
+  {
+    "revision": "d80b6c672fc519472fb4",
+    "url": "/mistercoinreact/static/js/main.d80b6c67.chunk.js"
+  },
+  {
+    "revision": "1c0e12060b985f0c392c",
+    "url": "/mistercoinreact/static/js/1.1c0e1206.chunk.js"
+  },
+  {
+    "revision": "9c10770b88a04b6ca9d7",
+    "url": "/mistercoinreact/static/js/runtime~main.9c10770b.js"
+  },
+  {
+    "revision": "49a9ad41116915da6becb2a10bbf444f",
+    "url": "/mistercoinreact/index.html"
+  }
+];
